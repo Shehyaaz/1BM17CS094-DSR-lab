b2f246350e965a05248de6444c4ad99292e11cbc
Shehyaaz <49763208+Shehyaaz@users.noreply.github.com>
2020-09-29 12:36:13 +0530

    lab 1 completed

---
+++ lab1/skn-lab1.r
@@ -0,0 +1,6 @@
+# 1)Write an R code to create a list of data objects of type Numeric, String, Real Numbers and name them.
+n1 <- c(10,20,30,40) # numeric object
+str1 <- c("Shehyaaz", "Khan", "Nayazi") # string object
+realnum <- c(9.64, 9.56, 9.8, 9.78) # real number object
+list1 <- list(n1, str1, realnum) # list object
+print(list1)
